801728ac76c1d6ba00a2fbb3178f3bce94d766e0
oranjacoby <93093702+oranjacoby@users.noreply.github.com>
2022-06-21 02:45:48 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask,url_for, redirect ,render_template, request,session
+
+app = Flask(__name__)
+app.secret_key = '123'
+
+users = {"user1": {"UserName": "erani","First Name": "Eran","Last Name": "Zehavi", "Email": "eranosh@gmail.com"},
+         "user2": {"UserName": "dori","First Name": "Dor", "Last Name": "micha", "Email": "dordor@gmail.com"},
+         "user3": {"UserName": "tali","First Name": "Tal", "Last Name": "Ben-Haim", "Email": "talben@gmail.com"},
+         "user4": {"UserName": "ori","First Name": "Ori", "Last Name": "malmilian", "Email": "malmil@gmail.com"},
+         "user5": {"UserName": "Ayelet", "First Name": "Ayelet", "Last Name": "Shaked", "Email": "ayeletsha@gmail.com"}
+         }
+
+@app.route('/home')
+@app.route('/')
+def main():
+    return render_template('Home Page.html')
+
+@app.route('/Contact')
+def contact():
+    return render_template('Contact.html')
+
+@app.route('/Home Page')
+def home():
+    return render_template('Home Page.html')
+
+@app.route('/assignment3_1')
+def assignment3_1():
+    hapoel = ("Yossi Abuksis", "Salim Toama", "Shalom Tikva", "Vincent Enyeama")
+    return render_template('assignment3_1.html', hapoel=hapoel)
+
+@app.route('/Contact Me')
+def Contact_Me():
+    return redirect('/Contact')
+
+@app.route('/Hapoel')
+def hapoel():
+    return redirect(url_for('home'))
+
+@app.route('/assignment3_2' , methods = ['GET','POST'])
+def assignment3_2():
+    current_method = request.method
+    if current_method == 'GET':
+        if 'user_name' in request.args:
+            user_name = request.args['user_name']
+            if user_name is '':
+                return render_template('assignment3_2.html', search=True, users=users, find=True)
+            user_dic = {}
+            for user in users.values():
+                if user['UserName'] == user_name:
+                    user_dic[1] = user
+            if len(user_dic) != 0:
+                return render_template('assignment3_2.html', search=True, find=True, users=user_dic)
+            else:
+                return render_template('assignment3_2.html', find=False, search=True)
+        return render_template('assignment3_2.html')
+    elif current_method == 'POST':
+        session['login'] = True
+        users[request.form['email']] = {'First Name': request.form['firstName'],
+                                            'Last Name': request.form['lastName'],
+                                            'Email': request.form['email'],
+                                            'User Name': request.form['userName']}
+        session['userName'] = request.form['userName']
+        return render_template('assignment3_2.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['login'] = False
+    return render_template('Home Page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
